1ea291249fb6a072efb954fc06a8cd9ab1b5c8c0
heru <bang.heru82@gmail.com>
2018-12-15 12:26:48 +0700

    add content_api app

--- content_feed/settings.py
+++ content_feed/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'content_api',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
